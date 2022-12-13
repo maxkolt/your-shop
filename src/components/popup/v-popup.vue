d41b37ceadb80694450a4d2309199a422bbde4ce
maximkoltovich <12345kolt@gmail.com>
2022-12-13 17:46:04 +0300

    1) Работаю над всплывающим модальным окном

---
+++ src/components/popup/v-popup.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="v-popup">
+  <div class="v-popup-header">
+    <span>Popup name</span>
+    <span>
+      <i class="material-icons">close</i>
+    </span>
+  </div>
+  <div class="v-popup-content">
+    <slot></slot>
+  </div>
+  <div class="v-popup-footer"></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "v-popup"
+}
+</script>
+
+<style>
+.v-popup {
+  padding: 16px;
+  position: fixed;
+  top: 50px;
+  width: 400px;
+  background: #ffffff;
+  box-shadow: 0 0 17px 0 #e7e7e7;
+  z-index: 10;
+}
+.v-popup-header, .v-popup-footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.v-popup-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './vuex/store'
-import router from "./router/router";
+import router from './router/router'
+import 'material-design-icons-iconfont'
 
 Vue.config.productionTip = false
 
